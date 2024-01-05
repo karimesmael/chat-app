8fe4ec7d7d3f190f50a1fb4c4050475d12aa03f8
karimesmael <karimesma3el407@gmail.com>
2024-01-05 18:25:13 +0200

    updated

--- frontend/src/components/chats/SingleChat.js
+++ frontend/src/components/chats/SingleChat.js
@@ -72,6 +72,10 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
       }
     });
     return () => {
+      socket.off("connected");
+      socket.off("message received");
+      socket.off("typing");
+      socket.off("stop typing");
       socket.disconnect();
     };
   });
